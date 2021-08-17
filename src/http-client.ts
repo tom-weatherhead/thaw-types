1b8e49e5490c3b9986707e3adbab9c0a579a476e
Tom Weatherhead <tomw@2hrd4u.org>
2021-08-17 07:57:15 -0600

    Dev

--- src/http-client.ts
+++ src/http-client.ts
@@ -4,10 +4,4 @@ import { Observable } from 'rxjs';
 
 export interface IHttpClient {
 	get(url: string): Observable<string>;
-	// post(url: string, body: unknown): Observable<unknown>;
-	// put(url: string, body: unknown): Observable<unknown>;
-	// patch(url: string, body: unknown): Observable<unknown>;
-	// delete<T>(url: string): Observable<T>;
-	// head<T>(url: string): Observable<T>;
-	// options<T>(url: string): Observable<T>;
 }
